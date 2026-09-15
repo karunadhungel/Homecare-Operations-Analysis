ee0e641dc1b3003d84e4a73f7b939db873ce85e7
karunadhungel <karunadhungel.5@gmail.com>
2026-09-15 12:05:39 -0400

    Create 01_create_table.sql

---
+++ SQL/01_create_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE homecare_operations (
+client_id TEXT,
+staff_id TEXT,
+service_type TEXT,
+region TEXT,
+scheduled_hours INTEGER,
+worked_hours INTEGER,
+visit_status TEXT,
+missed_visit TEXT,
+week INTEGER
+);
